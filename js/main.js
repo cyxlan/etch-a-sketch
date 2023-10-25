e4ec65d74839a413cb0d86233c43b566551e4bfe
cyxlan <49080055+cyxlan@users.noreply.github.com>
2023-10-25 17:21:13 -0400

    Add all 16x16 squares as grid

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const gridContainer = document.querySelector('.grid-container');
 
-// add 16 grid squares to page
-for (let i = 0; i < 16; i++) {
+// add 256 (16x16) grid squares to page
+for (let i = 0; i < 256; i++) {
   let gridSquare = document.createElement('div');
   gridSquare.classList.add('grid-square');
   gridContainer.appendChild(gridSquare);
